b18da1e0aae7d021f2dd5d01745e18997b293215
Murat Topuz <murattopuz@Murats-MBP.home>
2022-09-23 16:40:29 +0300

    complete a_customer_can_create_a_tarfin_card test

--- tests/Feature/TarfinCardControllerTest.php
+++ tests/Feature/TarfinCardControllerTest.php
@@ -4,26 +4,49 @@ declare(strict_types=1);
 
 namespace Tests\Feature;
 
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
+use Laravel\Passport\Passport;
 
 class TarfinCardControllerTest extends TestCase
 {
-    use RefreshDatabase;
+    use RefreshDatabase,WithFaker;
 
+    private string $api = 'api/tarfin-cards';
+    
     /**
      * @test
      */
     public function a_customer_can_create_a_tarfin_card(): void
     {
-        // 1. Arrange 🏗
-        // TODO:
+        $customer = User::factory()->create();
+        Passport::actingAs(
+            $customer,
+            ['create']
+        );
 
-        // 2. Act 🏋🏻‍
-        // TODO:
+        $creditCardType = $this->faker->creditCardType();
+        $response = $this->post($this->api,['type' => $creditCardType]);
 
-        // 3. Assert ✅
-        // TODO:
+        $response->assertCreated()->assertJson(fn (AssertableJson $json) =>
+        $json->has('data')->first(fn ($json) => 
+                $json->where('id', 1)
+                     ->where('type', $creditCardType)
+                     ->where('is_active', true)
+                     ->where('expiration_date', fn(string $date) => Carbon::now()->lt($date))
+                     ->where('number',function(int $number){
+                        if($number >= 1000000000000000 && $number <= 9999999999999999) {
+                            return true;
+                        }
+                        return false;
+                     })
+       ));
+        
+       $this->assertDatabaseHas('tarfin_cards',['user_id' => $customer->id,'type' => $creditCardType]);
     }
 
     /**
